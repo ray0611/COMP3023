6e75d8cd3bbd2e069d4a058ad425e9cd8b22beef
Ruohan Wang <rayyy020611@gmail.com>
2025-03-11 15:56:15 +1030

    add scanf and printf example: average of two numbers

--- Week1Project/Week1Project/main.cpp
+++ Week1Project/Week1Project/main.cpp
@@ -1,7 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    printf("Hello World! Two numbers are %d and %d",52,42 );
+    int n1, n2;
+    float avg;
+
+    printf("Enter two numbers separated by a space: ");
+    scanf("%d %d",&n1,&n2);
+    avg = (n1 + n2) / 2.0f;
+    printf("The average of %d and %d is %f ", n1, n2, avg);
+
+
     return 0;
 
 }
