7eddaf3bfd3e98115df60115bed700238d44dd63
Ruohan Wang <rayyy020611@gmail.com>
2025-03-11 16:17:20 +1030

    calculateAverage

--- Week1Project/Week1Project/main.cpp
+++ Week1Project/Week1Project/main.cpp
@@ -1,16 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+float calculateAverage(int m1, int m2);
+
 int main() {
     int n1, n2;
     float avg;
 
     printf("Enter two numbers separated by a space: ");
     scanf("%d %d",&n1,&n2);
-    avg = (n1 + n2) / 2.0f;
+    avg = calculateAverage(n1,n2);
     printf("The average of %d and %d is %f ", n1, n2, avg);
 
 
     return 0;
 
+}
+
+float calculateAverage(int m1, int m2) {
+    return (m1 + m2) / 2.0f;
 }
